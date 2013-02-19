65e4cfc3796483c2ecce7d09e49626890809ed2f
Mark Story <mark@mark-story.com>
2013-02-18 22:06:43 -0500

    Update code.

--- derp.py
+++ derp.py
@@ -1,4 +1,10 @@
-import os, re
+import os
+import re
+
 
 def fail():
     print 'this code has some fail in it, like this long long line'
+
+def more_fail(arg):
+    print 'this code is bad too';
+    print 'yep';

352144a64ffa8f51749953a79b71e2d609844fae
Mark Story <mark@mark-story.com>
2013-06-03 22:41:52 -0400

    Try and trigger a block diff problem

--- somefile.py
+++ somefile.py
@@ -5,6 +5,24 @@ import os
 log = logging.getLogger(__name__)
 
 
+def fixed_xor(left, right):
+    """
+    Returns the hex encoded XOR sum of two
+    equal length blocks of data.
+
+    >>> one = '1c0111001f010100061a024b53535009181c'.decode('hex')
+    >>> two = '686974207468652062756c6c277320657965'.decode('hex')
+    >>> fixed_xor(one, two)
+    '746865206b696420646f6e277420706c6179'
+    """
+    out = ''
+    for i in range(0, len(left)):
+        lft = ord(left[i])
+        rght = ord(right[i])
+        out += chr(lft ^ rght)
+    return out.encode('hex')
+
+
 def run_command(
         command,
         split=False,
@@ -13,14 +31,15 @@ def run_command(
     """
     Execute subprocesses.
     """
-    log.debug('Running %s', ' '.join(command))
-
-    env = os.environ.copy()
-
     if include_errors:
         error_pipe = subprocess.STDOUT
+        unused = 1
     else:
         error_pipe = subprocess.PIPE
+
+    env = {}
+    for key, value in os.environ.iteritems():
+        env[key] = value;
 
     process = subprocess.Popen(
         command,
